ca02cf31b82585d4288427836c3dc84095f309ec
Matt Wismer <mattwis86@gmail.com>
2015-11-17 20:04:16 -0500

    Use persistent to keep a HashMap of all games being played

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 ## Tak-server
 
-This is me playing with Iron, and an attempt to use tak-rs. Currently you play
-on localhost:8080, and pass in moves in the url, separated by slashes.
+This is me playing with Iron, and an attempt to use tak-rs. Currently you
+can view or create a game by going to localhost:8080/gameId, and you can make
+moves in the game by going to localhost:8080/gameId/move1/move2
 
-A game I just played against myself was: https://tak-server.herokuapp.com/e1F2/a1F1/c3F1/c2F2/b3F1/d3F2/b2F1/b4F2/c4F1/d4F2/c5F1/c2U1/b1F1/d4L1/b5F1/c3L01/c2F1/d3L1/d2F1/e2F2/e3F1/b3D01/d3F1/e2L1/a3F1/b4D1/c2U1/c3D112/c3F1/c4D01/a2F1/a4F2/a5F1/d4S2/d5C1/a4U1/e5F1/c3U12/a4F1/b2L11/b4F1/b3U11/c3F1
+A game I just played against myself was: http://localhost:8080/thisGame/e1F2/a1F1/c3F1/c2F2/b3F1/d3F2/b2F1/b4F2/c4F1/d4F2/c5F1/c2U1/b1F1/d4L1/b5F1/c3L01/c2F1/d3L1/d2F1/e2F2/e3F1/b3D01/d3F1/e2L1/a3F1/b4D1/c2U1/c3D112/c3F1/c4D01/a2F1/a4F2/a5F1/d4S2/d5C1/a4U1/e5F1/c3U12/a4F1/b2L11/b4F1/b3U11/c3F1
 
 I know this is a terrible idea, I just wanted to try something out. Better
 ideas for communication and such incoming.
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 extern crate tak;
 extern crate iron;
+extern crate persistent;
 
 use std::str::FromStr;
 use std::env;
+use std::collections::HashMap;
 use iron::prelude::*;
 use iron::status;
+use persistent::Write;
+use iron::typemap::Key;
 
 /// Look up our server port number in PORT, for compatibility with Heroku.
 fn get_server_port() -> u16 {
@@ -12,27 +16,46 @@ fn get_server_port() -> u16 {
     FromStr::from_str(&port_str).unwrap_or(8080)
 }
 
-fn main() {
-    Iron::new(|req: &mut Request| -> IronResult<Response> {
-        println!("{:?}", &req.url.path);
-        let mut game = tak::Board::new(5);
-        for turn in &req.url.path {
-            let formatted = format!("Illegal move: {}", turn);
-            let error = Response::with((status::Ok, formatted));
-            match turn.parse::<tak::Turn>() {
-                Ok(turn) => match tak::play(&turn, &mut game) {
-                    Ok(_) => (),
-                    Err(_) => return Ok(error),
-                },
+#[derive(Copy, Clone)]
+pub struct Games;
+
+impl Key for Games { type Value = HashMap<String, tak::Board>; }
+
+fn serve_game(req: &mut Request) -> IronResult<Response> {
+    println!("{:?}", &req.url.path);
+    let mutex = req.get::<Write<Games>>().unwrap();
+    let mut map = mutex.lock().unwrap();
+    let key = &req.url.path[0];
+
+    if !map.contains_key(key) {
+        map.insert((*key).clone(), tak::Board::new(5));
+    }
+    let mut game = map.get_mut(key).unwrap();
+
+    for turn in &req.url.path[1..] {
+        let formatted = format!("Illegal move: {}", turn);
+        let error = Response::with((status::Ok, formatted));
+        match turn.parse::<tak::Turn>() {
+            Ok(turn) => match tak::play(&turn, &mut game) {
+                Ok(_) => (),
                 Err(_) => return Ok(error),
-            }
+            },
+            Err(_) => return Ok(error),
         }
-        let response = match game.check_winner() {
-            Some(tak::Player::One) => format!("{}\n Player 1 Wins!", &game),
-            Some(tak::Player::Two) => format!("{}\n Player 2 Wins!", &game),
-            None => format!("{}", &game),
-        };
-        Ok(Response::with((status::Ok, response)))
-    }).http(("0.0.0.0", get_server_port())).unwrap();
+    }
+    let response = match game.check_winner() {
+        Some(tak::Player::One) => format!("{}\n Player 1 Wins!", &game),
+        Some(tak::Player::Two) => format!("{}\n Player 2 Wins!", &game),
+        None => format!("{}", &game),
+    };
+
+    Ok(Response::with((status::Ok, response)))
+}
+
+
+fn main() {
+    let mut chain = Chain::new(serve_game);
+    chain.link(Write::<Games>::both(HashMap::new()));
+    Iron::new(chain).http(("0.0.0.0", get_server_port())).unwrap();
     println!("On 3000");
 }
